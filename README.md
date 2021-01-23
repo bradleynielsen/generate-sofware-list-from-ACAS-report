e1cd31df277503a15a8dfc135635afc123930a25
Nielsen <Brad.Nielsen@serco-na.com>
2021-01-23 14:02:55 -0600

    adding msg box and auto folder gen

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # generate-sofware-list-from-ACAS-report
 Prototype python script to compile list of unique software identified by plugin 20811
+
+1. create a folder called 'csv' in root repo folder
+
+2. add ACAS csv files into ./csv
+
+3. run 'run.ps1' by right clicking and selecting "Run with PowerShell"
+
+
+
--- run.ps1
+++ run.ps1
@@ -1,6 +1,20 @@
-$scriptRoot = $PSScriptRoot
-$pythonFile = (ls $scriptRoot\*.py ).FullName
-$python     = (ls "$scriptRoot\python*\python.exe" ).FullName
+Add-Type -AssemblyName PresentationFramework
+$scriptRoot  = $PSScriptRoot
+$pythonFile  = (ls $scriptRoot\*.py ).FullName
+$python      = (ls "$scriptRoot\python*\python.exe" ).FullName
+$csvpath     = "$scriptRoot\csv"
+$resultspath = "$scriptRoot\results"
+
+if (!(test-path -path $csvpath)){
+    mkdir $csvpath
+    "Making csv folder"
+    [System.Windows.MessageBox]::Show('`csv` folder created. Add your ACAS reports to it before continuing.','Missing CSV files')
+}
+
+if (!(test-path -path $resultspath)){
+    mkdir $resultspath
+    "Making results folder"
+}
 
 & $python $pythonFile 
 
